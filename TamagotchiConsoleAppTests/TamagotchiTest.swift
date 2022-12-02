391a26204c1e35fc469a0591c92d8316c3650a62
Lysander Myrddin-Evans <myrddin-evans.l@etoncollege.org.uk>
2022-12-02 13:06:54 +0000

    made tests for extra functions in Tamagotchi class

--- TamagotchiConsoleAppTests/TamagotchiTest.swift
+++ TamagotchiConsoleAppTests/TamagotchiTest.swift
@@ -9,7 +9,7 @@ import XCTest
 
 class TamagotchiTest: XCTestCase {
     
-    func testWhenICreateATamagotchiItsWeightAndAgeAreInitialisedToZeroAndHungerToFive() {
+    func testWhenICreateATamagotchiItsWeightIsOneAndAgeIsZeroAndHungerIsFiveAndTirednessIsZero() {
         //arrange
         //act
         let tamagotchi = Tamagotchi()
@@ -17,6 +17,7 @@ class TamagotchiTest: XCTestCase {
         XCTAssertEqual(1, tamagotchi.getWeight())
         XCTAssertEqual(0, tamagotchi.getHappy())
         XCTAssertEqual(5, tamagotchi.getHunger())
+        XCTAssertEqual(0, tamagotchi.getTired())
     }
     
     func testEatingASnackDecreasesTamagotchiHungerByOneAndIncreasesHappyAndWeightByOne() {
@@ -25,12 +26,23 @@ class TamagotchiTest: XCTestCase {
         let expectedHunger = tamagotchi.getHunger() - 1
         let expectedHappy = tamagotchi.getHappy() + 1
         let expectedWeight = tamagotchi.getWeight() + 1
+        let expectedTiredness = tamagotchi.getTired() + 1
         //act
         tamagotchi.eatSnack()
         //assert
         XCTAssertEqual(expectedHappy, tamagotchi.getHappy())
         XCTAssertEqual(expectedHunger, tamagotchi.getHunger())
         XCTAssertEqual(expectedWeight, tamagotchi.getWeight())
+        XCTAssertEqual(expectedTiredness, tamagotchi.getTired())
     }
     
+    func testRunningSleepFunctionResetsTirednessToZero() {
+        // arrange
+        let tamagotchi = Tamagotchi()
+        // act
+        tamagotchi.eatSnack()
+        tamagotchi.sleep()
+        // assert
+        XCTAssertEqual(0, tamagotchi.getTired())
+    }
 }
